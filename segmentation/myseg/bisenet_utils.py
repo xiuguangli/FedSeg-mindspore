e36db12fe93c985d17da2467f381b09707ca9cd9
lxg <li.xgxv@gmail.com>
2025-11-30 19:23:16 +0800

    1

--- segmentation/myseg/bisenet_utils.py
+++ segmentation/myseg/bisenet_utils.py
@@ -126,7 +126,8 @@ def set_optimizer(model:nn.Cell, args):
         # params_list,
         model.trainable_params(),  # 如果参数分组有问题，可以直接传入所有可训练参数 
         # learning_rate=args.lr_scheduler_,  # 参数名从 lr 变为 learning_rate
-        learning_rate=args.lr,  # 参数名从 lr 变为 learning_rate 2ka
+        # learning_rate=args.lr,  # 参数名从 lr 变为 learning_rate 2ka
+        learning_rate=0.0002,  # 参数名从 lr 变为 learning_rate 2ka
         momentum=args.momentum,
         weight_decay=args.weight_decay,
     )
